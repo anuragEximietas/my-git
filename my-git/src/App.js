0dfa1b92dde98913b271f57d78739d65757dc9d2
Anurag Mishra <anurag.mishra@eximietas.design>
2024-01-07 13:23:34 +0530

    feat: Add list rendering functionality with three components

---
+++ my-git/src/App.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+
+function App() {
+  return (
+    <div>
+      <A />
+    </div>
+  );
+}
+
+function A() {
+  const [showB, setShowB] = useState(false);
+  const [showC, setShowC] = useState(false);
+  const [text, setText] = useState("");
+
+  const handleInputChange = (event) => {
+    setText(event.target.value);
+    setShowB(true);
+    setShowC(true);
+  };
+
+  return (
+    <div className="App">
+      <input type="text" onChange={handleInputChange} />
+      {showB && <B text={text} />}
+      {showC && <C text={text} />}
+    </div>
+  );
+}
+
+function B(props) {
+  const { text } = props;
+  return <h1>{text.toUpperCase()}</h1>;
+}
+
+function C(props) {
+  const { text } = props;
+  return <h1>{text.toLowerCase()}</h1>;
+}
+
+export default App;
